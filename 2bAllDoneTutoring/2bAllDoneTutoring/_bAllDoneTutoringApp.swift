5c6707ea6ecb46b267a2940041cfd931063371e5
Andrew Nyago <nyagoandrew@gmail.com>
2022-10-10 21:54:21 +0300

    Setup Project done

---
+++ 2bAllDoneTutoring/2bAllDoneTutoring/_bAllDoneTutoringApp.swift
@@ -0,0 +1,17 @@
+//
+//  _bAllDoneTutoringApp.swift
+//  2bAllDoneTutoring
+//
+//  Created by Andrew Nyago on 10/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct _bAllDoneTutoringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
