416daa80e8c912d334f56ea0a9015f99b4f661e1
Andrew Nyago <nyagoandrew@gmail.com>
2022-10-15 01:41:59 +0300

    Slightly differentt Firebase Swift configuration

--- 2bAllDoneTutoring/2bAllDoneTutoring/_bAllDoneTutoringApp.swift
+++ 2bAllDoneTutoring/2bAllDoneTutoring/_bAllDoneTutoringApp.swift
@@ -6,14 +6,28 @@
 //
 
 import SwiftUI
-import Firebase
+//import Firebase                                                   //for method 1.
+import FirebaseCore                                                 //for method 2
+
+class AppDelegate: NSObject, UIApplicationDelegate {                //ONLY FOR METHOD 2
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        
+        return true
+    }
+}
 
 @main
 struct _bAllDoneTutoringApp: App {
     
-    init(){
-        FirebaseApp.configure()
-    }
+    //    init(){                                                   //METHOD 1
+    //        FirebaseApp.configure()
+    //    }
+    
+    // register app delegate for Firebase setup                     //METHOD 2
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
